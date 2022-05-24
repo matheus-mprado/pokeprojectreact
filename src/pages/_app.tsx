64ea941dbd13114aee1c0dd4bcac886e02b9d140
Matheus Maximiliano <maximiliano.mprado@gmail.com>
2022-05-24 20:17:26 -0300

    feat: analytics add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,27 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 
 import "../styles/global.scss"
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+      <Script strategy="lazyOnload" id="analytics">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+        `}
+      </Script>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   )
 }
 
